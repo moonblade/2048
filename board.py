66424e67f01d24e85b53f787702d90df8b2139ce
moonblade <moonblade168@gmail.com>
2018-06-27 09:38:45 +0530

    board.py work started

---
+++ board.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import random
+from partialFormatter import fmt
+class Board():
+	# size - size of side of square board
+	# board - dictionary of cell -> value pairs
+	# fourProbability - probability of a 4 showing up instead of 2
+	def __init__(self, size=4, fourProbability=0.1):
+		self.size = size
+		self.board = dict((value,None) for value in [(x,y) for x in range(self.size) for y in range(self.size)])
+
+	def getEmptyCells(self):
+		return [x for x in self.board if self.board[x] == None]
+
+	def getEmptyCell(self):
+		if (len(self.getEmptyCells())==0):
+			return None
+		return random.choice(self.getEmptyCells())
+
+	def addData(self, cell, number):
+		if cell is None or self.board[cell] is not None:
+			return False
+		else:
+			self.board[cell] = number
+			return True
+
+	# return 2 or 4 based on probability
+	def getRandomData(self):
+		pass
+
+	def addRandomData(self):
+		pass
+
+	def print(self):
+		for x in range(self.size):
+			print(''.join([fmt.format('{0:<10}', self.board[key]) for key in self.board if key[0]==x]))
+
+b = Board(3)
+# print(b.size)
+# print(b.board)
+# print(b.getEmptyCell())
+b.addData(b.getEmptyCell(), random.randint(0, 10))
+b.addData(b.getEmptyCell(), random.randint(0, 10))
+b.addData(b.getEmptyCell(), random.randint(0, 10))
+b.addData(b.getEmptyCell(), random.randint(0, 10))
+b.addData(b.getEmptyCell(), random.randint(0, 10))
+b.print()
